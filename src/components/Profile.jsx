e5390b8b779b35c355bbe4d63459a9a95e806932
Vitaly <shishov_84@mail.ru>
2022-12-10 18:44:45 +0300

    add React

---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Profile () {
+    return (
+        <div className='content'>
+        <div>
+        <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            my post
+            <div>
+            New post
+            </div>
+        </div>
+        <div>
+            <div>
+            Post 1
+            </div>
+            <div>
+            Post 2
+            </div>
+            <div>
+            Post 3
+            </div>
+        </div>
+      
+        </div>
+    );
+}
+
+export default Profile;
