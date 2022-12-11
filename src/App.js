ad7889572a7a6ec5a60a789ffdab071389737598
Vitaly <shishov_84@mail.ru>
2022-12-11 20:48:52 +0300

    add Dialogs and add css in Api.css

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
 import Profile from './components/Profile/Profile';
-
-
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
-  return(
-    <div className='app-wrapper'>
-    <Header />
-    <Nav />
-    <Profile />    
-    </div>
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Nav/>
+            <div className= 'app-wrapper-content'>
+                <Dialogs/>
+                {/*<Profile />*/}
+            </div>
+        </div>
 
-  );
+    );
 
 }
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import classes from './Profile.module.css';
 
 function Profile () {
     return (
-        <div className={classes.content}>
+        <div>
         <div>
         <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
         </div>
