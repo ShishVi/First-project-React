ec22ac76821bc3c0cfd8347d3c8c29d957120d74
Vitaly <shishov_84@mail.ru>
2022-12-12 20:36:41 +0300

    add React Roter Dom and link add Navbar component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Setting from "./components/Setting/Setting";
 
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Nav/>
-            <div className= 'app-wrapper-content'>
-                <Dialogs/>
-                {/*<Profile />*/}
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Nav/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/dialog' element={<Dialogs/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/setting' element={<Setting/>}/>
+                    </Routes>
+                    
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
 
     );
 
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -4,11 +4,11 @@ import classes from './Nav.module.css';
 function Nav () {
     return (
         <nav className= {classes.nav}>
-            <div className={classes.item}><a>Profile</a></div>
-            <div className={`${classes.item} ${classes.active}`}><a>Messages</a></div>
-            <div className={classes.item}><a>News</a></div>
-            <div className={classes.item}><a>Music</a></div>
-            <div className={classes.item}><a>Setting</a></div>
+            <div className={classes.item}><a href='/profile'>Profile</a></div>
+            <div className={`${classes.item} ${classes.active}`}><a href='/dialog'>Messages</a></div>
+            <div className={classes.item}><a href='/news'>News</a></div>
+            <div className={classes.item}><a href='/music'>Music</a></div>
+            <div className={classes.item}><a href='/setting'>Setting</a></div>
         </nav>
     );
 }
