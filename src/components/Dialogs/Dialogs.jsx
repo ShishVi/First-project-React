483280762307e1b988c37f7e7daada697882fd43
Vitaly <shishov_84@mail.ru>
2022-12-13 21:16:20 +0300

    layout dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,31 @@ import React from "react";
 import classes from './Dialogs.module.css'
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItem}>
+                <div className={`${classes.dialog} ${classes.active}`}>
+                    Vitaly
+                </div>
+                <div className={classes.dialog}>
+                    Sergey
+                </div>
+                <div className={classes.dialog}>
+                    Dmitry
+                </div>
+                <div className={classes.dialog}>
+                    Irina
+                </div>
+                <div className={classes.dialog}>
+                    Alexsey
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello? My friend!</div>
+                <div className={classes.message}>Hi! How are you?</div>
+                <div className={classes.message}>I am Vitaly! What is your name?</div>
+                <div className={classes.message}>Good day</div>
+
+            </div>
         </div>
     );
 }
